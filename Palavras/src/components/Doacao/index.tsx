71cf44c9d50377ffbc0b05a99d53dcb371473ae4
unknown <eniofilho76@gmail.com>
2022-07-17 22:53:23 -0300

    paginas ajustadas para responsividade

--- Palavras/src/components/Doacao/index.tsx
+++ Palavras/src/components/Doacao/index.tsx
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react'
 import { Button, Card } from 'react-bootstrap'
 import "./styles.css"
-import ImgDoacao from "../../assets/images/doacao.png"
+import ImgDoacao from "../../assets/images/doacao.jpg"
 import Voluntarios from "../../assets/images/voluntarios.jpg"
+import Inscrever from "../../assets/images/inscrever.jpg"
 
 function Doacao() {
     return (
@@ -20,19 +21,33 @@ function Doacao() {
                                 A doação para Palavras de Paz é um passo em direção a um futuro melhor. Seu presente significa que podemos ajudar a moldar um mundo mais pacífico, uma pessoa de cada vez.
                                 <b>Você pode fazer parte da conquista pela paz.</b>
                             </Card.Text>
-                            <Button variant="outline-success">Faça sua Doação</Button>
+                            <Button variant="outline-success" style={{marginLeft: "50px"}}>Faça sua Doação</Button>
                         </Card.Body>
                     </Card>
                 </div >
-                <div className='cardDoisVoluntario'>
+                <div className='cardDoisInscrever'>
+                    <Card style={{ width: '18rem', height: "31rem" }}>
+                        <Card.Img variant="top" src={Inscrever} />
+                        <Card.Body>
+                            <Card.Title>Inscreva-se</Card.Title>
+                            <Card.Text>
+                            <b>Você está convidado a se inscrever.</b>
+                            Somente os participantes previamente inscritos poderão participar. É simples e rápido, basta optar pelas formas abaixo, e boa sorte.
+                            </Card.Text>
+                            <Button variant="outline-success" style={{marginLeft: "20px", marginRight: "15px", marginTop: "15px"}}>Whatsapp</Button>
+                            <Button variant="outline-success" style={{marginTop: "15px"}}>Inscrições</Button>
+                        </Card.Body>
+                    </Card>
+                </div >
+                <div className='cardTresVoluntario'>
                     <Card style={{ width: '18rem', height: "31rem" }}>
                         <Card.Img variant="top" src={Voluntarios} />
                         <Card.Body>
                             <Card.Title>Seja um Voluntário!</Card.Title>
                             <Card.Text>
-                            Precisamos de você para nos ajudar a promover a paz em todo o mundo. Faça parte do voluntariado: doe, socialize, faça compras, vá aos eventos, ou simplesmente assista aos vídeos.
+                            <b>Precisamos de você</b> para nos ajudar a promover a paz em todo o mundo. Faça parte do voluntariado: doe, socialize, faça compras, vá aos eventos, ou simplesmente assista aos vídeos.
                             </Card.Text>
-                            <Button variant="outline-success">Junte-se a nós</Button>
+                            <Button variant="outline-success" style={{marginLeft: "60px", marginTop: "15px"}}>Junte-se a nós</Button>
                         </Card.Body>
                     </Card>
                 </div>
--- Palavras/src/components/Zoon/index.tsx
+++ Palavras/src/components/Zoon/index.tsx
@@ -2,28 +2,29 @@ import React, { Fragment } from 'react'
 import "./styles.css"
 import Imagem from "../../assets/images/Zoom.jpg"
 import Blur from "../../assets/images/testeblur.png"
+import { Figure } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Zoom() {
     return (
         <Fragment>
 
-                <div className='headerZoom'></div>
+            <div className='headerZoom'></div>
             <div className='containerZoom'>
-
-
-                <div className='textoEImagem'>
-                    <div className='ajusteTextos'>
-                        <h2>Participe do curso online pelo Zoom</h2>
-                        <br></br>
-                        <p>Todas as aulas são transmitidas pela plataforma ZOOM e têm uma hora de duração, sendo   exibidos os vídeos mais reflexivos e inspiradores do Programa de Educação para Paz. Basta clicar no link ao lado às 19h30.
-                            Essa programação especial também será um bom treinamento a todos que queiram facilitar o Programa de Educação para Paz da Fundação Prem Rawat.
-                        </p>
-                    </div>
-                    <div className='imagemZoom'>
-                       <a href='https://zoom.us/s/293555970' > <img src={Imagem} alt="Logo do Zoom" style={{borderRadius: "10px"}} /></a>
-                    </div>
+                <div >
+                    <Figure >
+                        
+                        <Figure.Image style={{borderRadius: "10px", boxShadow: "2px 4px 6px black"}}
+                             width={400}
+                            height={100}
+                            alt="Imagem Zomm"
+                            src={Imagem}
+                        /></Figure>
                 </div>
-
+                <div className=' containerTextoZoon'>
+                    <h1>Participe do curso online pelo ZOOM</h1>
+                    <p> Todas as aulas são transmitidas pela plataforma<a href="https://zoom.us/s/293555970"> ZOOM</a> e têm uma hora de duração, sendo   exibidos os vídeos mais reflexivos e inspiradores do Programa de Educação para Paz. Basta acessar às 19h30 - <a href="https://zoom.us/s/293555970"> ZOOM </a>  Essa programação especial também será um bom treinamento a todos que queiram facilitar o Programa de Educação para Paz da Fundação Prem Rawat.</p>
+                </div>
 
 
             </div>
